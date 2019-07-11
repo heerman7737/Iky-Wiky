15c67121fab931bf45e16c4216c8d023b68b73a8
ktruo010 <ktruo010@gmail.com>
2019-07-11 14:41:56 -0700

    Problem with passing data

--- client/src/pages/Login/components/RegisterModal.js
+++ client/src/pages/Login/components/RegisterModal.js
@@ -3,6 +3,34 @@ import PropTypes from 'prop-types';
 import chatUtils from '../../../utils/chatUtils.js'
 
 class RegisterModal extends React.Component {
+
+  state = {
+    first_name: "",
+    last_name: "",
+    phone: "",
+    avatar: "", //url from AWS
+    username: "",
+    password: ""
+  }
+
+  handleAddUser = _ => {
+    chatUtils.addUser()
+    .then(({ data }) => this.setState({
+      first_name: this.first_name,
+      last_name: this.last_name,
+      phone: this.phone,
+      avatar: this.avatar, 
+      username: this.username,
+      password: this.password }))
+    .catch(error => console.log(error))
+  }
+
+  handleInputs = event => {
+    addEventListener.onKeyDown(event.target.value = this.setState(event.target.id))
+    .then(_ => sendStatus(200))
+    .catch(error => console.log(error))
+  }
+
   render() {
     // Render nothing if the "show" prop is false
     if(!this.props.show) {
@@ -30,32 +58,22 @@ class RegisterModal extends React.Component {
       padding: 30
     };
 
-    handleAddUser= _ => {
-      chatUtils.addUser()
-      .then()
-    }
+
 
     return (
       <div className="backdrop" style={{backdropStyle}}>
         <div className="modal" style={{modalStyle}}>
           {this.props.children}
           <form> 
-          <input
-        placeholder="First Name"/>
-                <input
-        placeholder="Last Name"/>
-        <input
-        placeholder="Username"/>
-        <input
-        placeholder="Password"/>
-        <input
-        placeholder="Phone Number"/>
-        <button onClick="handleAddUser">Submit</button>
+          <input onKeyDown={handleInputs} id="first_name" placeholder="First Name"/>
+        <input onKeyDown={handleInputs} id="last_name" placeholder="Last Name"/>
+        <input onKeyDown={handleInputs} id="username" placeholder="Username"/>
+        <input onKeyDown={handleInputs} id="password" placeholder="Password"/>
+        <input onKeyDown={handleInputs} id="phone" placeholder="Phone Number"/>
+        <button onClick={this.handleAddUser}>Submit</button>
         </form>
           <div className="footer">
-            <button onClick={this.props.onClose}>
-              Close
-            </button>
+            <button onClick={this.props.onClose}>Close</button>
           </div>
         </div>
       </div>
