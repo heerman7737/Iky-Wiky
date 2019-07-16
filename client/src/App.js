8ca55b09afa1a759686914af42ae1ebf08a96c25
Erica P <erica.parra@rocketmail.com>
2019-07-16 16:45:49 -0700

    login changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { BrowserRouter , Route } from 'react-router-dom'
 import Login from './pages/Login'
 import './App.css'
-// import Session from './pages/Session'
+import Profile from './pages/Profile'
+import Session from './pages/Session'
 
 const App = _ => {
   
@@ -10,9 +11,9 @@ const App = _ => {
   return(
     <BrowserRouter>
       <div className="App">
-        <Route path='/Login' component = { _ => <Login/> }/>
-        {/* <Route path='/Session' component = { _ => <Sessions/> } /> */}
-        
+        <Route exact path='/' component = { _ => <Login/> }/>
+        <Route path='/Session' component = { _ => <Session/> } />
+        <Route path='/Profile' component = { _ => <Profile/> } />
       </div>
     </BrowserRouter>
   )
