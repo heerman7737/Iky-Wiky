39afc3f3f7a949b4a17778fc6598b30ed9878192
Giang <giang.to.7737@gmail.com>
2019-07-16 16:38:34 -0700

    Login

--- client/src/pages/Login/components/LoginModal.js
+++ client/src/pages/Login/components/LoginModal.js
@@ -1,27 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import chatUtils from '../../../utils/chatUtils.js'
+import axios from 'axios'
+import Chatkit from '@pusher/chatkit-client'
 
 class LoginModal extends React.Component {
 
   state = {
     username: "",
     password: "",
-    first_name: "",
-    last_name: "",
-    phone: "",
-    avatar: "", //url from AWS
-    _id: ""
   }
   
-  handlePullUserData = _ => {
-    _.preventDefault()
+  handlePullUserData = e => {
+    e.preventDefault()
     chatUtils.login(this.state.username,this.state.password)
     .then((data) =>{
-      console.log(data)
-      localStorage.setItem("userId",data.data[0]._id)})
+      localStorage.setItem("userId",data.data[0]._id)
+      localStorage.setItem("user_name",data.data[0].username)
+    })
     .then(console.log("Working"))
     .catch(error => console.log(error))
+
+    
+    let userId= localStorage.getItem("userId")
+    let user_name= localStorage.getItem("user_name")
+    console.log('user '+userId)
+    axios.post('http://localhost:3001/user',{userId,user_name})
+    .then(()=>{
+      console.log(userId,user_name)
+      const tokenProvider = new Chatkit.TokenProvider({
+        url: 'http://localhost:3001/authenticate',
+      });
+      const chatManager = new Chatkit.ChatManager({
+        instanceLocator: 'v1:us1:366d4bfd-9da9-4a3c-8b98-fb24d065efc5',
+        userId,
+        tokenProvider
+      });
+      return chatManager.connect()
+      .then(currentUser => {
+        console.log('Successful connection', currentUser)
+      })
+      .catch(err => {
+        console.log('Error on connection', err)
+      })
+    })
   }
   handleInputs = event => {
     this.setState({
