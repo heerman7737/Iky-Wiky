cae11335a52ad62ac038efe706275b0261223166
Giang <giang.to.7737@gmail.com>
2019-07-18 18:23:08 -0700

    messaging

--- client/src/components/SessionComp/SendMessageForm.js
+++ client/src/components/SessionComp/SendMessageForm.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
 
- class ChatSession extends Component {
+ class SendMessageForm extends Component {
    constructor(props) {
      super(props)
      this.state = {
@@ -18,28 +18,46 @@ import React, { Component } from 'react'
 
    onChange(e) {
      this.setState({ text: e.target.value })
-     console.log(e.target.value )
      if (this.props.onChange) {
        this.props.onChange()
      }
    }
 
    render() {
+     const styles = {
+       container: {
+         padding: 20,
+         borderTop: '1px #4C758F solid',
+         marginBottom: 20,
+       },
+       form: {
+         display: 'flex',
+       },
+       input: {
+         color: 'inherit',
+         background: 'none',
+         outline: 'none',
+         border: 'none',
+         flex: 1,
+         fontSize: 16,
+       },
+     }
      return (
-       <>
+       <div style={styles.container}>
          <div>
-           <form onSubmit={this.onSubmit}>
+           <form onSubmit={this.onSubmit} style={styles.form}>
              <input
                type="text"
                placeholder="Type a message here then hit ENTER"
                onChange={this.onChange}
                value={this.state.text}
+               style={styles.input}
              />
            </form>
          </div>
-       </>
+       </div>
      )
    }
  }
 
- export default ChatSession
+ export default SendMessageForm
--- client/src/pages/Session/Session.js
+++ client/src/pages/Session/Session.js
@@ -1,50 +1,98 @@
 import React, { Component } from 'react'
-import ChatSession from '../../components/SessionComp/ChatSession'
 import Chatkit from '@pusher/chatkit-client'
-// import Room from '../../components/SessionComp/Room'
-class Session extends Component {
-
-    state = {
-        currentUser:{},
-        currentRoom:{},
-        message:[]
+import MessageList from '../../components/SessionComp/MessageList'
+import SendMessageForm from '../../components/SessionComp/SendMessageForm'
+class ChatScreen extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        currentUser: {},
+        currentRoom: {},
+        messages: []
     }
-componentDidMount(){
-    let userId=localStorage.getItem("userId")
-    const tokenProvider = new Chatkit.TokenProvider({
+    this.sendMessage = this.sendMessage.bind(this)
+  }
+  sendMessage(text) {
+       this.state.currentUser.sendMessage({
+         text,
+         roomId: this.state.currentRoom.id,
+       })
+     }
+  componentDidMount () {
+    let userId= localStorage.getItem("userId")
+
+    const chatManager = new Chatkit.ChatManager({
+      instanceLocator: 'v1:us1:366d4bfd-9da9-4a3c-8b98-fb24d065efc5',
+      userId,
+      tokenProvider: new Chatkit.TokenProvider({
         url: 'http://localhost:3001/authenticate',
-      });
-      const chatManager = new Chatkit.ChatManager({
-        instanceLocator: 'v1:us1:366d4bfd-9da9-4a3c-8b98-fb24d065efc5',
-        userId,
-        tokenProvider
-      });
-      chatManager.connect()
-      .then(currentUser=>{
-        currentUser.sendSimpleMessage({
-            roomId: '20091598',
-            text: "Bye bYe",
-          })
-          .then(messageId => {
-            console.log(`Added message to ${messageId}`)
-          })
-          .catch(err => {
-            console.log(`Error adding message to ${err}`)
-          })
-      })
-}
-handleInputs = event => {
-    this.setState({
-      [event.target.id]: event.target.value
+      }),
     })
- }
-    render() {
-        return (
-            <>
-                <ChatSession/>
-            </>
-        )
-    }
+
+    chatManager
+      .connect()
+      .then(currentUser => {
+        this.setState({ currentUser })
+        return currentUser.subscribeToRoom({
+            roomId: '20091598',
+            messageLimit: 100,
+            hooks: {
+                onNewMessage: message => {
+                this.setState({
+                messages: [...this.state.messages, message],
+            })
+          },
+        },
+      })
+    })
+          .then(currentRoom => {
+                this.setState({ currentRoom })
+               })
+      .catch(error => console.error('error', error))
+  }
+
+  render() {
+    const styles = {
+      container: {
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      chatContainer: {
+        display: 'flex',
+        flex: 1,
+      },
+      whosOnlineListContainer: {
+        width: '300px',
+        flex: 'none',
+        padding: 20,
+        backgroundColor: '#2c303b',
+        color: 'white',
+      },
+      chatListContainer: {
+        padding: 20,
+        width: '85%',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+   }
+    return (
+      <div style={styles.container}>
+        <div style={styles.chatContainer}>
+          <aside style={styles.whosOnlineListContainer}>
+            <h2>Who's online PLACEHOLDER</h2>
+          </aside>
+          <section style={styles.chatListContainer}>
+          <MessageList
+              messages={this.state.messages}
+              style={styles.chatList}
+            />     
+             <SendMessageForm onSubmit={this.sendMessage} />
+          </section>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default Session
+export default ChatScreen
