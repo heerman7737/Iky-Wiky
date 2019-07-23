cc424d2f26623a4d50ec2d0a78da573aa93bd320
Giang <giang.to.7737@gmail.com>
2019-07-22 22:23:40 -0700

    direct chat finished with changing room and autoscroll

--- client/src/components/SessionComp/MessageList.js
+++ client/src/components/SessionComp/MessageList.js
@@ -4,9 +4,19 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Avatar from '@material-ui/core/Avatar';
-
+import ReactDOM from 'react-dom'
 class MessagesList extends Component {
- 
+  componentWillUpdate() {
+    const node = ReactDOM.findDOMNode(this)
+    this.shouldScrollToBottom = node.scrollTop + node.clientHeight  +30>= node.scrollHeight
+}
+
+componentDidUpdate() {
+    if (this.shouldScrollToBottom) {
+        const node = ReactDOM.findDOMNode(this)
+        node.scrollTop = node.scrollHeight   
+    }
+}
   render() {
   
     // const fromMe = this.props.fromMe ? 'from-me' : '';
