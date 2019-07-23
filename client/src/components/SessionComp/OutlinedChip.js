fcf8a4a56ba735ad44145dd94ede542564970281
Erica P <erica.parra@rocketmail.com>
2019-07-23 16:27:14 -0700

    styling added to user and chat drawers

--- client/src/components/SessionComp/OutlinedChip.js
+++ client/src/components/SessionComp/OutlinedChip.js
@@ -3,6 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Chip from '@material-ui/core/Chip';
 import FaceIcon from '@material-ui/icons/Face';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,9 +22,24 @@ class OutlinedChipItem extends Component{
   render(){
     return(
       <>
-      <ul>
-        <button name={this.props.name} id={this.props.id} onClick={this.props.onClick}>{this.props.children}</button>
-        </ul>
+      <List style={{marginLeft:'10px' , overflowY: 'auto'}}>
+        <ListItem>
+        <Chip 
+        avatar={
+          <Avatar>
+            <FaceIcon />
+          </Avatar>
+        }
+        
+        // {<Avatar>{this.props.avatar}</Avatar>}
+        name={this.props.name} 
+        id={this.props.id} 
+        onClick={this.props.onClick}
+        label={this.props.children}
+        variant="outlined"
+        />
+        </ListItem>
+      </List>
       </>
     )
   }
